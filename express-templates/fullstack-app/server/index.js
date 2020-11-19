89c41b225ee631522486036e6d09c5fc420e0caa
david-osas <azamegbeosarumense@gmail.com>
2020-11-19 16:53:24 +0100

    Add comments to fullstack-app server index.js

--- express-templates/fullstack-app/server/index.js
+++ express-templates/fullstack-app/server/index.js
@@ -1,10 +1,16 @@
-const express = require('express')
+// import server modules
+const express = require('express');
 const path = require('path');
-const app = express()
 
+//initialze server app
+const app = express();
+
+//set middlewares to send static files and parse data being exchanged between front and back ends
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.json(), express.urlencoded({extended: false}));
 
+
+//home route to send default html file
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 
@@ -12,13 +18,13 @@ app.get('/', (req,res) => {
 
 
 
-
+//set port variable for either production or development environments
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 5000;
 }
 
-
+//listen to http requests made to server
 app.listen(port, () => {
   console.log('server started on port 5000');
 })
